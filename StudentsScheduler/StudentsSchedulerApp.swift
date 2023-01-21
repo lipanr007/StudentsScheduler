82182232005d726e1e2ddddcebd481b05aed5db5
rares <rrsed123@gmail.com>
2023-01-21 17:50:06 +0200

    Initial Commit

---
+++ StudentsScheduler/StudentsSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentsSchedulerApp.swift
+//  StudentsScheduler
+//
+//  Created by rares on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct StudentsSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
