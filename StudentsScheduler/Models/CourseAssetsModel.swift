f317438aacb28c785065b9d6aedc54dd8933b504
rares <rrsed123@gmail.com>
2023-03-11 11:25:57 +0200

    Modified the Models: UserModel, CourseAssetsModel

--- StudentsScheduler/Models/CourseAssetsModel.swift
+++ StudentsScheduler/Models/CourseAssetsModel.swift
@@ -5,29 +5,70 @@
 //  Created by rares on 22.01.2023.
 //
 
-import Foundation
+import SwiftUI
 
 struct CourseAssets: Codable {
-    var notes: [String]
+    var notes: [Note]
     var deadlines: [Deadline]
+    var homework: [Homework]
+    var exams: [Exam]
     var grades: [Grade]
     
-    init(notes: [String], deadlines: [Deadline], grades: [Grade]) {
+    
+    init(notes: [Note], deadlines: [Deadline], homework: [Homework], exams: [Exam], grades: [Grade]) {
         self.notes = notes
         self.deadlines = deadlines
+        self.homework = homework
+        self.exams = exams
         self.grades = grades
     }
 }
 
-class Deadline: Codable {
+struct Deadline: Codable {
     var day: Date
     var time: Date
-    var subject: String
+    var courseName: String
     
     init(day: Date, time: Date, subject: String) {
         self.day = day
         self.time = time
-        self.subject = subject
+        self.courseName = subject
+    }
+}
+
+struct Note: Codable {
+    var title: String
+    var content: String
+    
+    init(title: String, content: String) {
+        self.title = title
+        self.content = content
+    }
+}
+
+struct Homework: Codable {
+    var description: String
+    var deadline: Deadline
+    
+    init(description: String, deadline: Deadline){
+        self.description = description
+        self.deadline = deadline
+    }
+}
+
+struct Exam: Codable {
+    var date: Date
+    var grade: Grade
+    var status: ExamStatus
+    
+    enum ExamStatus: String, CaseIterable, Codable {
+        case passes, failed
+    }
+    
+    init(date: Date, grade: Grade, status: ExamStatus) {
+        self.date = date
+        self.grade = grade
+        self.status = status
     }
 }
 
--- StudentsScheduler/Models/UserModel.swift
+++ StudentsScheduler/Models/UserModel.swift
@@ -5,17 +5,21 @@
 //  Created by rares on 21.01.2023.
 //
 
-import Foundation
+import SwiftUI
 
 class User: Identifiable, Codable {
     var id = UUID()
     var username: String
     var password: String
+    var homeAddress: String
+    var universityAddress: String
     var courses: [Course]
     
-    init(username: String, password: String, courses: [Course]) {
+    init(username: String, password: String, homeAddress: String, universityAddress: String, courses: [Course]) {
         self.username = username
         self.password = password
+        self.homeAddress = homeAddress
+        self.universityAddress = universityAddress
         self.courses = courses
     }
 }
